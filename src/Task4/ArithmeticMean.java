bd1565253cac505883f25eddd54ca17d4dd4a194
Pustik41 <Pustik41@gmail.com>
2016-05-20 16:18:07 +0300

    New item

--- src/Task4/ArithmeticMean.java
+++ src/Task4/ArithmeticMean.java
@@ -7,6 +7,7 @@ public class ArithmeticMean {
         int[] array = new int[10];
         int sum = 0;
         int aMean = 0;
+        int graph = 0;
         
         for(int i = 0; i < array.length; i++){
             array[i] = (int) (Math.random()* 100);
